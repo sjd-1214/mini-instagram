bff0484f2b4c5d44c4000257b13daba9a0a0257e
Sajjd Siddiqui <f230620@cfd.nu.edu.pk>
2024-12-01 16:29:40 +0500

    Validations

---
+++ validations.h
@@ -0,0 +1,7 @@
+
+#include <iostream>
+using namespace std;
+bool validateusername(string username);
+bool validate_strong_password(string password);
+bool validate_email(string email);
+bool validate_DOB(string dob);
